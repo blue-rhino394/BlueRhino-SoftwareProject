94c53f848c1131ac3d3a8228df4d4cffd96b8e69
Iseeicy <iseeicyhawkston@gmail.com>
2020-09-16 22:00:49 -0500

    Imported base project

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+﻿import { testServer } from './testServer';
+const commandLineArgs = require('command-line-args');
+
+
+
+
+//
+//  Options
+//
+
+// Define what command line arguments we want, and what type we expect
+// them to be.
+const optionDefinitions = [
+    { name: 'port', type: Number }
+];
+
+// Store 'em in a generic object, then explicitly retrieve them
+const options = commandLineArgs(optionDefinitions);
+const port = options.port || 30330;
+
+
+
+
+//
+//  App Execution
+//
+
+const server = new testServer(port);
+
+server.listen(portUsed => {
+    console.log(`Server is listening on port ${portUsed}!`);
+});
