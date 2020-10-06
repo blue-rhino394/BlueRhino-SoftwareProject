8a31c3d7c60304f640b253d287be4783cff2147c
Iseeicy <iseeicyhawkston@gmail.com>
2020-10-05 22:03:07 -0500

    Created tem[;ate fpr cardREST definitions

---
+++ src/cardREST.ts
@@ -0,0 +1,76 @@
+﻿import express, { Application } from "express";
+
+export function defineCardREST(app: Application): void {
+
+    app.post('/api/get-card', (req, res) => {
+
+        // TODO - IMPLEMENT!
+
+        // Get dummy data
+        const responseData: any = undefined;
+        res.send(responseData);
+    });
+
+    app.post('/api/set-card', (req, res) => {
+
+        // TODO - IMPLEMENT!
+
+        // Get dummy data
+        const responseData: any = undefined;
+        res.send(responseData);
+    });
+
+    app.post('/api/delete-card', (req, res) => {
+
+        // TODO - IMPLEMENT!
+
+        // Get dummy data
+        const responseData: any = undefined;
+        res.send(responseData);
+    });
+
+    app.post('/api/get-saved-cards', (req, res) => {
+
+        // TODO - IMPLEMENT!
+
+        // Get dummy data
+        const responseData: any = undefined;
+        res.send(responseData);
+    });
+
+    app.post('/api/toggle-save', (req, res) => {
+
+        // TODO - IMPLEMENT!
+
+        // Get dummy data
+        const responseData: any = undefined;
+        res.send(responseData);
+    });
+
+    app.post('/api/toggle-favorite', (req, res) => {
+
+        // TODO - IMPLEMENT!
+
+        // Get dummy data
+        const responseData: any = undefined;
+        res.send(responseData);
+    });
+
+    app.post('/api/set-memo', (req, res) => {
+
+        // TODO - IMPLEMENT!
+
+        // Get dummy data
+        const responseData: any = undefined;
+        res.send(responseData);
+    });
+
+    app.post('/api/search-card', (req, res) => {
+
+        // TODO - IMPLEMENT!
+
+        // Get dummy data
+        const responseData: any = undefined;
+        res.send(responseData);
+    });
+}
