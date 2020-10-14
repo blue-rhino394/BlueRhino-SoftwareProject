5082b1982e0ef159fdddef81277196c804f2009a
brijen1 <bpatel3293@gmail.com>
2020-10-13 22:25:46 -0500

    update jest

--- src/test/dummyData.test.js
+++
@@ -1,6 +0,0 @@
-const dummyData = require('./dummyData')
-
-test('Create dummy post data indicating that we have toggled a card as saved'), ()=>
-{
-    expect(dummyData.getDummySavedCard).toReturn(); 
-}
