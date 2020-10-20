acd8507caea904ed474e836ad1df54864f0b0748
Iseeicy <iseeicyhawkston@gmail.com>
2020-10-19 20:53:01 -0500

    Revert "Created generic cache manager, restructured old one into databaseCacheManager"
    This reverts commit f24c95f72a817b5845da6a82735bdf6c84312070.

--- src/cacheManager.ts
+++ src/cacheManager.ts
@@ -1,15 +1,19 @@
-﻿
+﻿import { user } from "./user";
+import { card } from "./card";
 
-export class cacheManager<K, V> {
 
-    private collection: Map<K, V>;
+export class cacheManager {
+
+    private userCollection: Map<string, user>;
+    private cardCollection: Map<string, card>;
+
 
     //
     //  Constructor and Initialization
     //
 
     constructor() {
-        this.collection = new Map<K, V>();
+
     }
 
 
@@ -17,62 +21,52 @@ export class cacheManager<K, V> {
 
 
 
-    // Check within cache to see if the specified key exists in it already
-    public keyExists(key: K): boolean {
-        return this.collection.has(key);
+    //
+    //  User Methods
+    //
+
+    public userExists(uuid: string): boolean {
+        // TODO
+        return false;
     }
 
-    // Get the specified value from the cache using a key.
-    // If the key does not exist in the cache, return null
-    public getValue(key: K): V {
-
-        // If this key does not exist in the cache, bail out.
-        if (!this.keyExists(key)) {
-            return null;
-        }
-
-        // Otherwise
-        // Return the value!
-
-        // TODO - setup update timeout function
-
-        return this.collection.get(key);
+    public getUser(uuid: string): user {
+        // TODO
+        return null;
     }
 
-    // Add the specified value to the cache.
-    // If a user by this ID already exists in the cache, return false.
-    // Otherwise, return true.
-    public addValue(keyToUse: K, valueToAdd: V): boolean {
-
-        // If this key is already in the cache, bail out
-        if (this.keyExists(keyToUse)) {
-            return false;
-        }
-
-        // Otherwise...
-        // Add the value to the cache
-        this.collection.set(keyToUse, valueToAdd);
-
-        // TODO - set up start timeout function
-
-        return true;
+    public addUser(userToAdd: user): void {
+        // TODO
     }
 
-    // Removes the specified value from the cache using a key.
-    // If there's not a value by that key in the manager, return false.
-    // Otherwise, return true.
-    public removeValue(key: K): boolean {
+    public removeUser(uuid: string): void {
+        // TODO
+    }
 
-        // If this key is not in the cache, bail out
-        if (!this.keyExists(key)) {
-            return false;
-        }
 
-        // Otherwise...
-        // Remove the value from the cache
-        this.collection.delete(key);
 
-        // TODO - setup stop timeout function if timeout exists
-        return true;
+
+
+
+    //
+    //  Card Methods
+    //
+
+    public cardExists(cardID: string): boolean {
+        // TODO
+        return false;
+    }
+
+    public getCard(cardID: string): card {
+        // TODO
+        return null;
+    }
+
+    public addCard(cardToAdd: card): void {
+        // TODO
+    }
+
+    public removeCard(cardID: string): void {
+        // TODO
     }
 }
