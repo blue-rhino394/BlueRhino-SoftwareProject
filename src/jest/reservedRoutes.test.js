41d7fbdb587610cb844e73d221b33527e972521f
brijen1 <bpatel3293@gmail.com>
2020-10-27 16:58:09 -0500

    Update reservedRoutes.test.js

--- src/jest/reservedRoutes.test.js
+++ src/jest/reservedRoutes.test.js
@@ -7,9 +7,13 @@ test("Adds a route to this object", () =>{
 
 test(" Removes a route from this object", () =>{
     expect(Route.removeRoute).toHaveReturned();
+}
+)
 
 test("Looks to see if the provided route is in this object",()=>
 {
     expect(Route.hasRoute).toHaveReturned();
 
-})
+}
+)
+
