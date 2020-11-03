4fcea7d75d2d0aff854db07275154385c07ebf4f
mahmed61 <mustafa09876@gmail.com>
2020-11-03 14:33:39 -0600

    Update card.test.ts

--- src/jest/card.test.ts
+++ src/jest/card.test.ts
@@ -6,200 +6,275 @@ import { cardSchema } from "../interfaces/cardSchema";
 import { cardContent } from "../interfaces/cardContent";
 
 
-    // The user to test with
-    //      POPULATE IN BEFORE ALL
-    //      REMOVED IN AFTER ALL
-    var testUser: user = undefined;
+// The user to test with
+//      POPULATE IN BEFORE ALL
+//      REMOVED IN AFTER ALL
+var testUser: user = undefined;
 
 
-    // Before any tests execute...
+// Before any tests execute...
+beforeAll(async () => {
+
+    //
+    //  Register test user
+    //
+
+    // Create user account schema
+    const newUserAccount: userAccountSchema = {
+        email: "fakeemailthatshouldneverexist@brhino.org",
+        passwordHash: "blablabla",
+        public: {
+            firstName: "Test",
+            lastName: "User",
+            customURL: "fakeemailthatshouldneverexistslug",
+            profilePictureURL: "https://ui-avatars.com/api/?name=Joe+Mama&format=png&font-size=0.33&rounded=true&size=300&bold=true&color=FFFFF&background=29b6f6"
+        }
+    }
+
+    // Actually register the test user
+    testUser = await databaseWrapper.createUser(newUserAccount);
+
+    // If we failed to create a testing user...
+    if (!testUser) {
+        throw new Error("Failed to create testing user...");
+    }
+});
+
+afterAll(async () => {
+
+    //
+    //  Remove Test User
+    //
+
+    // Remove the test user from the database
+    await databaseWrapper.deleteUser(testUser.getUUID());
+});
+
+describe("Get Testing Constructor Input Schema", () => {
+    //
+    //
+    //
+    var testSchema: cardSchema = undefined;
+
+    const newcardSchema: cardSchema = {
+        cardID: "003261564641",
+        ownerID: "0032610452564642",
+        ownerInfo: {
+            firstName: "Test",
+            lastName: "User",
+            customURL: "fakeemailthatshouldneverexistslug",
+            profilePictureURL: "https://ui-avatars.com/api/?name=Joe+Mama&format=png&font-size=0.33&rounded=true&size=300&bold=true&color=FFFFF&background=29b6f6"
+        },
+        content: undefined,
+        stats: undefined
+    } 
+
     beforeAll(async () => {
+        testSchema = newcardSchema;
+    });
 
-        //
-        //  Register test user
-        //
+    test("Ensure that getID is equal to cardID of constructor's input schem", async () => {
+        
+    });
 
-        // Create user account schema
-        const newUserAccount: userAccountSchema = {
-            email: "fakeemailthatshouldneverexist@brhino.org",
-            passwordHash: "blablabla",
-            public: {
-                firstName: "Test",
-                lastName: "User",
-                customURL: "fakeemailthatshouldneverexistslug",
-                profilePictureURL: "https://ui-avatars.com/api/?name=Joe+Mama&format=png&font-size=0.33&rounded=true&size=300&bold=true&color=FFFFF&background=29b6f6"
-            }
+    test("Ensure getOwnerUUID() is equal to CardID of constructor input schema", async () => {
+
+    });
+
+    test("Ensure that ownerInfo is equal to ownerInfo of constructor's input schema", async () => {
+
+    });
+
+    test("Ensure that return is equal to constructor's input schema", async () => {
+
+    });
+
+    test("Ensure that return is equal to content of constructor's input schema", async () => {
+
+    });
+
+    test("Ensure that return is equal to content.layout of constructor's input schema", async () => {
+
+    });
+
+    test("Ensure that return is equal to stats of constructor's input schema", async () => {
+
+    });
+
+});
+
+
+describe("Get Card Testing", () => {
+
+    //
+    //  Settings
+    //
+
+    // The card to test with
+    var testCard: card = undefined;
+
+    // The content used to create it
+    const content: cardContent = {
+        published: false,
+        tags: ["Testing Card", "Super Cool"],
+        socialMediaLinks: [],
+        cardProperties: [],
+        layout: {
+            background: "#c7ddff",
+            fontColor: "#05152e"
         }
+    }
 
-        // Actually register the test user
-        testUser = await databaseWrapper.createUser(newUserAccount);
 
-        // If we failed to create a testing user...
-        if (!testUser) {
-            throw new Error("Failed to create testing user...");
+
+    //
+    //  Setup / Teardown
+    //
+
+    // Create testCard before tests run
+    beforeAll(async () => {
+        testCard = await databaseWrapper.createCard(testUser.getUUID(), content);
+
+        // If we couldn't create a card for some reason...
+        if (!testCard) {
+            throw new Error("Failed to create testing card...");
         }
     });
 
+    // Destroy testCard after tests finish
     afterAll(async () => {
-
-        //
-        //  Remove Test User
-        //
-
-        // Remove the test user from the database
-        await databaseWrapper.deleteUser(testUser.getUUID());
+        await databaseWrapper.deleteCard(testUser.getCardID());
+        await testUser.setCardID("");
     });
 
 
 
+    //
+    //  Tests
+    //
 
-
-
-
-    describe("Get Card Testing", () => {
-
-        //
-        //  Settings
-        //
-
-        // The card to test with
-        var testCard: card = undefined;
-
-        // The content used to create it
-        const content: cardContent = {
-            published: false,
-            tags: ["Testing Card", "Super Cool"],
-            socialMediaLinks: [],
-            cardProperties: [],
-            layout: {
-                background: "#c7ddff",
-                fontColor: "#05152e"
-            }
-        }
-
-
-
-        //
-        //  Setup / Teardown
-        //
-
-        // Create testCard before tests run
-        beforeAll(async () => {
-            testCard = await databaseWrapper.createCard(testUser.getUUID(), content);
-
-            // If we couldn't create a card for some reason...
-            if (!testCard) {
-                throw new Error("Failed to create testing card...");
-            }
+    describe("Test getID()", () => {
+        test("Ensure getID() returns something not empty", async () => {
+            expect(testCard.getID()).toBeTruthy();
         });
 
-        // Destroy testCard after tests finish
-        afterAll(async () => {
-            await databaseWrapper.deleteCard(testUser.getCardID());
-            await testUser.setCardID("");
+        test("Ensure getID() does not change", async () => {
+            const getID1 = testCard.getID();
+            const getID2 = testCard.getID();
+            expect(getID1).toEqual(getID2);
+        });
+
+    });
+
+    describe("Test getOwnerUUID()", () => {
+        test("Ensure getOwnerUUID() returns something not empty", async () => {
+            expect(testCard.getOwnerUUID()).toBeTruthy();
+        });
+
+        test("Ensure getOwnerUUID() does not change", async () => {
+            const getOwnerUUID1 = testCard.getOwnerUUID();
+            const getOwnerUUID2 = testCard.getOwnerUUID();
+            expect(getOwnerUUID1).toEqual(getOwnerUUID2);
+        });
+
+    });
+
+    describe("Test getCardSchema()", () => {
+        test("Ensure getCardSchema() returns something not empty", async () => {
+            expect(testCard.getCardSchema()).toBeTruthy();
+        });
+
+        test("Ensure cardID returns something not empty", async () => {
+            expect(testCard.getCardSchema().cardID).toBeTruthy();
+        });
+
+        test("Ensure ownerID returns something not empty", async () => {
+            expect(testCard.getCardSchema().ownerID).toBeTruthy();
+        });
+
+        test("Ensure ownerinfo returns something not empty", async () => {
+            expect(testCard.getCardSchema().ownerInfo).toBeTruthy();
+        });
+
+        test("Ensure content returns something not empty", async () => {
+            expect(testCard.getCardSchema().content).toBeTruthy();
+        });
+
+        test("Ensure stats returns something not empty", async () => {
+            expect(testCard.getCardSchema().stats).toBeTruthy();
+        });
+
+        test("Ensure ownerID is equal to getID()", async () => {
+            const recievedcardID = testCard.getCardSchema().cardID;
+            const recievedgetID = testCard.getID();
+            expect(recievedcardID).toEqual(recievedgetID);
+        });
+
+        test("Ensure ownerID is equal to getOwnerUUID()", async () => {
+            const recievedownerID = testCard.getCardSchema().ownerID;
+            const recievedOwnerUUID = testCard.getOwnerUUID();
+            expect(recievedownerID).toEqual(recievedOwnerUUID);
+        });
+
+        test("Ensure content is equal to getCardContent()", async () => {
+            const recievedcontent = testCard.getCardSchema().content;
+            const recievedCardContent = testCard.getCardContent();
+            expect(recievedcontent).toEqual(recievedCardContent);
+        });
+
+        test("Ensure stats is equal to getCardStats()", async () => {
+            const recievedstats = testCard.getCardSchema().stats;
+            const recievedCardStats = testCard.getCardStats();
+            expect(recievedstats).toEqual(recievedCardStats);
         });
 
 
+    });
 
-        //
-        //  Tests
-        //
-
-        describe("Test getID()", () => {
-            test("Ensure getID() returns something not empty", async () => {
-                expect(testCard.getID()).toBeTruthy();
-            });
-
-            test("Ensure getID() does not change", async () => {
-                
-            });
-
-            test("Ensure getID() is equal to CardID of constructor input schema", async () => {
-                
-            });
-
+    describe("Test getCardContent()", () => {
+        test("Ensure getCardContent() returns something not empty", async () => {
+            expect(testCard.getCardContent()).toBeTruthy();
         });
 
-        describe("Test getOwnerUUID()", () => {
-            test("Ensure getOwnerUUID() returns something not empty", async () => {
-                expect(testCard.getOwnerUUID()).toBeTruthy();
-            });
-
-            test("Ensure getOwnerUUID() does not change", async () => {
-
-            });
-
-            test("Ensure getOwnerUUID() is equal to CardID of constructor input schema", async () => {
-
-            });
-
+        test("Ensure published is defined", async () => {
+            expect(testCard.getCardContent().published).toBeDefined();
         });
 
-        describe("Test getCardSchema()", () => {
-            test("Ensure getCardSchema() returns something not empty", async () => {
-                expect(testCard.getCardSchema()).toBeTruthy();
-            });
-
-            test("Ensure cardID returns something not empty", async () => {
-                expect(testCard.getCardSchema().cardID).toBeTruthy();
-            });
-
-            test("Ensure ownerID returns something not empty", async () => {
-                expect(testCard.getCardSchema().ownerID).toBeTruthy();
-            });
-
-            test("Ensure ownerinfo returns something not empty", async () => {
-                expect(testCard.getCardSchema().ownerInfo).toBeTruthy();
-            });
-
-            test("Ensure content returns something not empty", async () => {
-                expect(testCard.getCardSchema().content).toBeTruthy();
-            });
-
-            test("Ensure stats returns something not empty", async () => {
-                expect(testCard.getCardSchema().stats).toBeTruthy();
-            });
-
-            test("Ensure ownerID is equal to getOwnerUUID()", async () => {
-                const recievedownerID = testCard.getCardSchema().ownerID;
-                const recievedOwnerUUID = testCard.getOwnerUUID();
-                expect(recievedownerID).toEqual(recievedOwnerUUID);
-            });
-
-            test("Ensure content is equal to getCardContent()", async () => {
-                const recievedcontent = testCard.getCardSchema().content;
-                const recievedCardContent = testCard.getCardContent();
-                expect(recievedcontent).toEqual(recievedCardContent);
-            });
-
-            test("Ensure stats is equal to getCardStats()", async () => {
-                const recievedstats = testCard.getCardSchema().stats;
-                const recievedCardStats = testCard.getCardStats();
-                expect(recievedstats).toEqual(recievedCardStats);
-            });
-
-            test("Ensure that ownerInfo is equal to ownerInfo of constructor's input schema", async () => {
-                const recievedownerinfo = testCard.getCardSchema().ownerInfo;
-                
-            });
-
+        test("Ensure tags is truthy", async () => {
+            expect(testCard.getCardContent().tags).toBeTruthy();
         });
 
-        describe("Test getCardContent()", () => {
-
+        test("Ensure socialMediaLinks is truthy", async () => {
+            expect(testCard.getCardContent().socialMediaLinks).toBeTruthy();
         });
 
-        describe("Test getCardLayout()", () => {
-
+        test("Ensure cardProperties is truthy", async () => {
+            expect(testCard.getCardContent().cardProperties).toBeTruthy();
         });
 
-        describe("Test getCardStats()", () => {
+        test("Ensure layout is truthy", async () => {
+            expect(testCard.getCardContent().layout).toBeTruthy();
+        });
+
+        test("Ensure layout is equal to getCardLayout()", async () => {
+            const recievedCardLayout = testCard.getCardLayout();
+            const recievedlayout = testCard.getCardContent().layout;
+            expect(recievedCardLayout).toEqual(recievedlayout);
+        });
+
+    });
+
+    describe("Test getCardLayout()", () => {
+
+    });
+
+    describe("Test getCardStats()", () => {
             
-        });
+    });
 
        
         
-    });
+});
 
 
 
