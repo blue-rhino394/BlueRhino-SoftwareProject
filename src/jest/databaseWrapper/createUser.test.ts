099f2497f233f4c7dd8656710222195b13c8789b
Iseeicy <iseeicyhawkston@gmail.com>
2020-11-05 20:45:03 -0600

    Refactored utility methods into their own file

--- src/jest/databaseWrapper/createUser.test.ts
+++ src/jest/databaseWrapper/createUser.test.ts
@@ -1,7 +1,7 @@
 ﻿import { databaseWrapper } from "../../databaseWrapper";
 import { userAccountSchema } from "../../interfaces/userAccountSchema";
-import { v4 } from "uuid";
 import { user } from "../../user";
+import { generateRandomUserAccountSchema } from "./utilityMethods";
 
 
 
@@ -197,22 +197,3 @@ describe("databaseWrapper.createUser()", () => {
 
 
 
-
-//
-//  Utility Functions
-//
-
-// Generates a random userAccountSchema using a bunch of randomly
-// generated UUIDs.
-function generateRandomUserAccountSchema(): userAccountSchema {
-    return {
-        email: `${v4()}@${v4()}.com`,
-        passwordHash: v4(),
-        public: {
-            firstName: `firstName${v4()}`,
-            lastName: `lastName${v4()}`,
-            customURL: `slug${v4()}`,
-            profilePictureURL: "https://ui-avatars.com/api/?name=Test+User&format=png&font-size=0.33&rounded=true&size=300&bold=true&color=FFFFF&background=29b6f6"
-        }
-    }
-}
